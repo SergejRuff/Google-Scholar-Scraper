7b50616f4dfe43367d80ebfd7259d73930025704
SergejRuff <serijnh@gmail.com>
2024-04-02 14:39:48 +0200

    test-function

--- code/test_link.R
+++ code/test_link.R
@@ -1,3 +1,4 @@
+library(RSelenium)
 rm(list=ls())
 
 ## This version of the function doesnt require the user to sepcify number of pages he wants to scrape.
@@ -83,7 +84,6 @@ rotate_proxy <- function() {
   current_proxy <<- sample(proxies, 1)
 }
 
-
 retry_request <- function(url, proxies) {
   max_proxies <- length(proxies)
   shuffled_proxies <- sample(proxies)
@@ -91,51 +91,65 @@ retry_request <- function(url, proxies) {
   for (proxy_index in 1:max_proxies) {
     current_proxy <- shuffled_proxies[proxy_index]
 
-
     # Split current_proxy into IP address and port
     proxy_parts <- unlist(strsplit(current_proxy, ":"))
     proxy_ip <- proxy_parts[1]
     proxy_port <- as.numeric(proxy_parts[2])
 
     tryCatch({
-      session <- rvest::session(url, httr::use_proxy(url = proxy_ip, port = proxy_port))
+      # Initialize RSelenium
+      driver <- rsDriver(browser = "firefox", port = 4445L)
+      remote_driver <- driver[["client"]]
+
+      # Set proxy for the session
+      proxy_info <- paste0(proxy_ip, ":", proxy_port)
+      remote_driver$addProxy(type = "manual", httpProxy = proxy_info, httpsProxy = proxy_info)
+
+      # Navigate to the URL
+      remote_driver$navigate(url)
+
       # If the connection is successful, return the URL and port of the proxy
       return(list(url = proxy_ip, port = proxy_port))
     }, error = function(e) {
       cat("Error occurred with proxy", current_proxy, ":", conditionMessage(e), "\n")
+      # Close the driver if an error occurs
+      driver$server$stop()
     })
   }
   cat("All proxies failed. Failed to establish connection.\n")
   return(NULL)
 }
 
-scrape_gs <- function(term,proxies,...) {
+scrape_gs <- function(term, proxies, ...) {
   library(rvest)
-  library(httr)
-  library(magrittr)
+  library(RSelenium)
   library(stringr)
   library(Randomuseragent)
-  library(RSelenium)
-
-
 
   gs_url_base <- "https://scholar.google.com/scholar?q="
   term <- gsub("^mmu-", "", term)
-  #term <- paste0("Virus AND intitle:",term)
-  print(paste0("intitle:",term))
 
-  best_proxy <- retry_request(paste0(gs_url_base, URLencode(paste0("intitle:",term))), proxies)
-  gs_page <- rvest::session(paste0(gs_url_base, URLencode(paste0("intitle:",term))), httr::use_proxy(url = best_proxy[[1]], port = best_proxy[[2]]))
-  #gs_page <- rvest::session(paste0(gs_url_base, URLencode(term)), httr::use_proxy(url = proxy_ip, port = proxy_port))
+  print(paste0("intitle:", term))
 
-  cat(paste("current link ->",paste0(gs_url_base, URLencode(paste0("intitle:",term)))),"\n")
+  best_proxy <- retry_request(paste0(gs_url_base, URLencode(paste0("intitle:", term))), proxies)
 
-  # Read the HTML content of the Google Scholar search results page
-  page <- read_html(gs_page,user_agent=Randomuseragent::random_useragent())
+  # Initialize RSelenium
+  driver <- rsDriver(browser = "firefox", port = 4445L)
+  remote_driver <- driver[["client"]]
+
+  # Set proxy for the session
+  proxy_info <- paste0(best_proxy$url, ":", best_proxy$port)
+  remote_driver$addProxy(type = "manual", httpProxy = proxy_info, httpsProxy = proxy_info)
+
+  # Navigate to the URL
+  remote_driver$navigate(paste0(gs_url_base, URLencode(paste0("intitle:", term))))
+
+  # Extract page content
+  page_html <- remote_driver$getPageSource()[[1]]
+  page <- read_html(page_html, encoding = "UTF-8")
 
   # Extract the entire page content as text
   page_text <- as.character(page)
-  #print(page_text)
 
   # Extract numbers within <span> tags from the text
   numbers <- str_extract_all(page_text, "span>(\\d+)") %>%
@@ -145,52 +159,50 @@ scrape_gs <- function(term,proxies,...) {
 
   # Find the maximum number
   max_number <- max(numbers, na.rm = TRUE)
+
   if (is.infinite(max_number)) {
     cat("Max number is -Inf. Trying with a new proxy.\n")
     rotate_proxy()  # Rotate to a new proxy
     return(scrape_gs(term, proxies))  # Retry scraping with a new proxy
   }
-  cat(paste0(paste0("intitle:",term)," has ",max_number, " pages with results\n"))
 
-
-  # set httr config outside of function and use them inside ...; e.g.:
-  # useragent <- httr::user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36") # spoof user agent
-  # proxy <- httr::use_proxy(url = "proxy.com", port = 8080, username = "dave", password = "pass", auth = "basic")
+  cat(paste0(paste0("intitle:", term), " has ", max_number, " pages with results\n"))
 
   result_list <- list()
   i <- 1
 
-  for (n_page in 0:(max_number- 1)*10) {  # gs page indexing starts with 0; there are 10 articles per page, see "?start=" param
-    gs_url <- paste0(gs_url_base, "&start=", n_page, "&q=", noquote(gsub("\\s+", "+", trimws(paste0("intitle:",term)))))
-    cat(paste(gs_url,"-> page:",n_page/10 + 1),"\n")
+  for (n_page in 0:(max_number - 1) * 10) {
+    gs_url <- paste0(gs_url_base, "&start=", n_page, "&q=", noquote(gsub("\\s+", "+", trimws(paste0("intitle:", term)))))
+    cat(paste(gs_url, "-> page:", n_page / 10 + 1), "\n")
 
     t0 <- Sys.time()
-    session <- rvest::session(gs_url, httr::use_proxy(url = best_proxy[[1]], port = best_proxy[[2]]))
-    #cat(paste0("current http status is ",session$response$status_code))
-    t1 <- Sys.time()
-    response_delay <- as.numeric(t1-t0)  # backing off time
-    wbpage <- rvest::read_html(session,user_agent=Randomuseragent::random_useragent())
+    # Navigate to the next page
+    remote_driver$navigate(gs_url)
 
-    #crawl_delay <- sample(2:10, 1)
+    # Extract page content
+    page_html <- remote_driver$getPageSource()[[1]]
+    wbpage <- read_html(page_html, encoding = "UTF-8")
+
+    t1 <- Sys.time()
+    response_delay <- as.numeric(t1 - t0)  # backing off time
     crawl_delay <- 3
 
-
     # Avoid HTTP error 429 due to too many requests - use crawl delay & back off
-    Sys.sleep(crawl_delay + 3*response_delay + runif(n = 1, min = 0.5, max = 1))
-    if((i %% 10) == 0) {  # sleep every 10 iterations
+    Sys.sleep(crawl_delay + 3 * response_delay + runif(n = 1, min = 0.5, max = 1))
+
+    if ((i %% 10) == 0) {  # sleep every 10 iterations
       message("taking a break")
-      Sys.sleep(10 + 10*response_delay + runif(n = 1, min = 0, max = 1))
+      Sys.sleep(10 + 10 * response_delay + runif(n = 1, min = 0, max = 1))
     }
     i <- i + 1
 
     # Raw data
-    titles <- rvest::html_text(rvest::html_elements(wbpage, ".gs_rt"))
-    #print(titles)
-    authors_years <- rvest::html_text(rvest::html_elements(wbpage, ".gs_a"))
-    part_abstracts <- rvest::html_text(rvest::html_elements(wbpage, ".gs_rs"))
-    bottom_row_nodes <- rvest::html_elements(wbpage, ".gs_fl")
+    titles <- html_text(html_nodes(wbpage, ".gs_rt"))
+    authors_years <- html_text(html_nodes(wbpage, ".gs_a"))
+    part_abstracts <- html_text(html_nodes(wbpage, ".gs_rs"))
+    bottom_row_nodes <- html_nodes(wbpage, ".gs_fl")
     bottom_row_nodes <- bottom_row_nodes[!grepl("gs_ggs gs_fl", as.character(bottom_row_nodes), fixed = TRUE)] # exclude the ones with this tag, they are download links
-    bottom_row <- rvest::html_text(bottom_row_nodes)
+    bottom_row <- html_text(bottom_row_nodes)
 
     # Processed data
     authors <- gsub("^(.*?)\\W+-\\W+.*", "\\1", authors_years, perl = TRUE)
@@ -204,7 +216,7 @@ scrape_gs <- function(term,proxies,...) {
       result_list,
       list(
         list(
-          page = n_page/10 + 1,
+          page = n_page / 10 + 1,
           term = term,
           title = titles,
           authors = authors,
@@ -219,23 +231,12 @@ scrape_gs <- function(term,proxies,...) {
   # Return as data frame
   result_df <- lapply(result_list, as.data.frame)
   result_df <- as.data.frame(do.call(rbind, result_df))
-  result_df<- result_df[!grepl("\\b(?:[[:alpha:]]*omas?|cancer|leukemia)\\b", result_df$title, ignore.case = TRUE), ]
+  result_df <- result_df[!grepl("\\b(?:[[:alpha:]]*omas?|cancer|leukemia)\\b", result_df$title, ignore.case = TRUE), ]
+
+  # Stop RSelenium driver
+  remote_driver$close()
 
   return(result_df)
 }
 
-#test <- scrape_gs("mmu-miR-196b-5p",proxies)
-#print(test)
-
-testcsv <- read.csv("/home/sergej/Desktop/coding/viper/data/intercept/intersection_3.csv")
-
-search_results <- list()
-
-for (i in 1:length(testcsv$MiRNA)) {
-
-  search_results[[i]] <-scrape_gs(testcsv$MiRNA[i],proxies)
-}
-#[1:2,]
-
-
-
+test <- scrape_gs("mmu-miR-196b-5p",proxies)
